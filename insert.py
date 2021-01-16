cbaabbea50dfe677140a4afa8c7d914b7343afbb
aleksa3123 <68758086+aleksa3123@users.noreply.github.com>
2021-01-16 01:02:38 +0100

    Update insert.py

--- insert.py
+++ insert.py
@@ -1,5 +1,9 @@
-import mysql.connector
+import requests
 import json
+import textwrap
+import base64
+
+from bs4 import BeautifulSoup
 from colorama import Fore, Back, Style
 
 
@@ -12,38 +16,64 @@ ______ _ _
 \_|   |_|_|_| |_| |_|\___/_/\_\\
                                                                                                   
 VenoxZero je radio traku!
-
 """
 print(Fore.RED + filmox )
-host_input = input(Fore.YELLOW + 'Unesite ip adresu databaze: ')
-user_input = input(Fore.YELLOW + 'Unesite korisnicko ime: ')
-passwd_input = input(Fore.YELLOW + 'Unesite sifru: ')
-database_input = input(Fore.YELLOW + 'Unesite ime baze: ')
+broj = input(Fore.YELLOW + 'Unesite broj strana: ') #Input za unosenje broja stranica
+
+stranice = int(broj) + 1 #Dodavanje 1 na broj stranica da bi broj bio tacan
+
+def write_json(data,filename="sadrzaj.json"): #Definisanje json fajla
+    with open(filename, "w") as f: 
+        json.dump(data, f,  indent=4)
 
 
+with open("sadrzaj.json") as json_file: #Otvaranje json fajla
+    data = json.load(json_file) #Ocitavanje podataka iz json fajla
+
+    
+
+    for i in range(1, int(stranice)):#For loop za broj stranica
+
+        URL = 'https://www.popcornsrbija.com/filmovi/?page=' + str(i) #URL sa kog su stranice, njega mozete menjati ali paginacija mora biti preko get metode(Ako je ?page drugacije promenite ga)
+        page = requests.get(URL)
+
+        soup = BeautifulSoup(page.content, 'html.parser')
 
 
-mysql=mysql.connector.connect(
-    host = host_input, 
-    user = user_input, 
-    passwd = passwd_input,
-    database = database_input
+        for opis in soup.find_all('div', class_='opis'): #Pronalazi sve div tagove sa klasom opis
+            title =  opis.find('a')
+            link = 'https://www.popcornsrbija.com' + title['href'] #Pravljenje linka za film
+            page = requests.get(link)
 
-)
-with open('sadrzaj.json') as json_file:
-    data = json.load(json_file)
-    for p in data['filmovi']:
-        naslov = p['naslov']
-        iframe = p['iframe']
-        opis = p['opis']
-        mycursor = mysql.cursor()
-        sql = "INSERT INTO filmovi(naslov, iframe, opis) values(%s,%s, %s)"
-        val = (naslov, iframe, opis)
+            soup = BeautifulSoup(page.content, 'html.parser')
+           
 
-        mycursor.execute(sql,val)
+            results = soup.find('body')
 
-mysql.commit();
+            #Definisanje promenljivih
+            title = results.find('h1')
+            opis = results.find('p')
+            iframe = results.find('iframe')
+            temp = data["filmovi"]
+            
+            naslov = title.text
+            naslov_r = naslov[:naslov.find('(')].strip()
 
 
+            #Ubacivanje podataka u json fajl
+            y = {
+                
+            "url" : link ,
+            "naslov": str(naslov_r),
+            "opis": str(opis.text),
+            "iframe": str(iframe)
+            
+            }
 
+            temp.append(y)
+            print(naslov_r)
+            
 
+write_json(data)
+print("\n")
+print("Uspesno ste sacuvali sav sadrzaj!")
